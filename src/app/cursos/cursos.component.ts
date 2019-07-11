265e305a4a02e44f9159fa51a736ca343f775f43
03fred <fred.rocke02@gmail.com>
2019-07-11 13:09:38 -0300

    queryParam pagenation

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CursoService } from './curso.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cursos',
@@ -8,10 +10,28 @@ import { CursoService } from './curso.service';
 })
 export class CursosComponent implements OnInit {
 cursos: any[];
-  constructor(private cursoService: CursoService) { }
+pagina: number;
+inscricao: Subscription;
+
+
+  constructor(private cursoService: CursoService,
+    private route: ActivatedRoute,
+    private router:  Router) { }
 
   ngOnInit() {
     this.cursos = this.cursoService.getCursos();
+
+    this.inscricao = this.route.queryParams.subscribe((queryParams: any) => {
+        this.pagina = queryParams['pagina'];
+      }
+    )
+  }
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
   }
 
+  proximaPagina(){
+    this.router.navigate(['/cursos'],
+    {queryParams:{'pagina':++this.pagina}})
+  }
 }
